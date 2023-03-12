373bc3bd0c46128ce11874eaf6878be4d5bc818e
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-12 16:01:30 -0400

    [ADD] add view to list all course

--- universidad/Aplicaciones/Academico/views.py
+++ universidad/Aplicaciones/Academico/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Curso
+
 
 # Create your views here.
+def home(request):
+    cursos= Curso.objects.all()
+    return render(request, "gestionCurso.html", {"listado":cursos})
