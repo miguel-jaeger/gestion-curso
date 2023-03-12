e892f82f5e66f94da0044d8ff8c62926fbc5eecd
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-12 15:08:52 -0400

    [FIX] fix name of project

--- universidad/universidad/wsgi.py
+++ universidad/universidad/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'universidad.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Universidad.settings')
 
 application = get_wsgi_application()
