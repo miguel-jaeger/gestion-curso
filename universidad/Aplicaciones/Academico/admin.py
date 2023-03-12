c6a53089ad8decc97d2d1dbf9af7cd42a02689c2
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-12 15:31:33 -0400

    [UPDATE] admin display

--- universidad/Aplicaciones/Academico/admin.py
+++ universidad/Aplicaciones/Academico/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from Aplicaciones.Academico import models
+from .models import Curso
 # Register your models here.
+#admin.site.register(Curso)
+
+@admin.register(models.Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['codigo','nombre', 'creditos']
+    search_fields = ['codigo', 'nombre']
+    # inlines = [NoteInline]
+    # autocomplete_fields = ['nombre']
--- universidad/Aplicaciones/Academico/models.py
+++ universidad/Aplicaciones/Academico/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class Curso(models.Model):
     codigo = models.CharField(primary_key=True, max_length=6)
     nombre= models.CharField(max_length=50)
-    creditos=models.PositiveSmallIntegerField() 
+    creditos=models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        texto = "{0}" "({1})"
+        return texto.format(self.nombre,self.creditos)
