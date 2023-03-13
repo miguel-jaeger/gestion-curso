23ec022c16de635201ae0e79fe17a14ddb87fed7
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-13 01:40:37 -0400

    [CHANGE] models fields change

--- universidad/Aplicaciones/Academico/models.py
+++ universidad/Aplicaciones/Academico/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Curso(models.Model):
-    codigo = models.CharField(primary_key=True, max_length=6)
+    #codigo = models.CharField(max_length=60, unique=True)
+    #codigo = models.CharField(primary_key=True, max_length=6)
+    codigo = models.CharField(max_length=60, unique=True)
     nombre= models.CharField(max_length=50)
     creditos=models.PositiveSmallIntegerField()
 
