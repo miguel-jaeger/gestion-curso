bbabbf4d4242dcf75feecd5af83592d8effaa304
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-13 00:31:18 -0400

    [CHANGE] add url to search course, separate register course to list

--- universidad/Aplicaciones/Academico/urls.py
+++ universidad/Aplicaciones/Academico/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('registrar/', views.addCursoF),
     path('registrarCurso/', views.addCurso),
     path('editarCurso/<codigo>', views.editCurso),
     path('editaCurso/', views.editCursoPost),
     path('eliminarCurso/<codigo>', views.deleteCurso),
+    path('buscar', views.buscarCurso),
 ]
