d9a623ebb3d4877de0e24a754a2c03acc9a13082
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-14 13:55:30 -0400

    [ADD] add translate data table

--- universidad/Aplicaciones/Academico/static/js/gestionCurso.js
+++ universidad/Aplicaciones/Academico/static/js/gestionCurso.js
@@ -13,6 +13,11 @@
 })();
 
 jQuery(document).ready(function () {
+
+    
+
+//Spanish
+
     const dataOptions={
         columnDefs:[
             {className:"centered", targets:[0,1,2,3,4]},
@@ -22,9 +27,33 @@ jQuery(document).ready(function () {
         pageLength:5,
         lengthMenu: [
             [5,10, 25, -1],
-            [5, 10, 25, 'All'],
+            [5, 10, 25, 'Todos'],
         ],
         destroy:true,
+        language: {
+            "sProcessing":    "Procesando...",
+            "sLengthMenu":    "Mostrar _MENU_ registros",
+            "sZeroRecords":   "No se encontraron resultados",
+            "sEmptyTable":    "Ningún dato disponible en esta tabla",
+            "sInfo":          "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":     "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "sInfoFiltered":  "(filtrado de un total de _MAX_ registros)",
+            "sInfoPostFix":   "",
+            "sSearch":        "Buscar:",
+            "sUrl":           "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":    "Último",
+                "sNext":    "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
     };
 
     jQuery('#example').DataTable(dataOptions);
