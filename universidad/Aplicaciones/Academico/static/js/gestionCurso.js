ed59f81353541f8e3386b3b38966eeef8ea5dacf
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-14 13:47:50 -0400

    [CHANGE] add datatable list

--- universidad/Aplicaciones/Academico/static/js/gestionCurso.js
+++ universidad/Aplicaciones/Academico/static/js/gestionCurso.js
@@ -11,3 +11,21 @@
     });
 
 })();
+
+jQuery(document).ready(function () {
+    const dataOptions={
+        columnDefs:[
+            {className:"centered", targets:[0,1,2,3,4]},
+            {orderable:false, targets:[4]},
+            {searchable:false, targets:[4]},
+        ],
+        pageLength:5,
+        lengthMenu: [
+            [5,10, 25, -1],
+            [5, 10, 25, 'All'],
+        ],
+        destroy:true,
+    };
+
+    jQuery('#example').DataTable(dataOptions);
+});
