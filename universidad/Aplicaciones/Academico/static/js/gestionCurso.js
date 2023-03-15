424380440fc8dfb90786218592a9c17e4c51894d
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-15 14:38:11 -0400

    [FIX] fix translations

--- universidad/Aplicaciones/Academico/static/js/gestionCurso.js
+++ universidad/Aplicaciones/Academico/static/js/gestionCurso.js
@@ -35,9 +35,10 @@ jQuery(document).ready(function () {
             "sLengthMenu":    "Mostrar _MENU_ registros",
             "sZeroRecords":   "No se encontraron resultados",
             "sEmptyTable":    "Ningún dato disponible en esta tabla",
-            "sInfo":          "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-            "sInfoEmpty":     "Mostrando registros del 0 al 0 de un total de 0 registros",
-            "sInfoFiltered":  "(filtrado de un total de _MAX_ registros)",
+            "sInfo":          "Mostrando _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":     "Mostrando  0 de un total de 0 registros",
+            /*"sInfoFiltered":  "(filtrado de un total de _MAX_ registros)",*/
+            "sInfoFiltered":  "",
             "sInfoPostFix":   "",
             "sSearch":        "Buscar:",
             "sUrl":           "",
