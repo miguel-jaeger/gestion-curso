ec1eb21ab45474d2f58a38ddc4f2012679007cd9
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-23 17:15:03 -0400

    [CHANGE] add url to add, list and delete user

--- universidad/Aplicaciones/Academico/urls.py
+++ universidad/Aplicaciones/Academico/urls.py
@@ -3,10 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('cursos/', views.cursos),
     path('registrar/', views.addCursoF),
     path('registrarCurso/', views.addCurso),
     path('editarCurso/<codigo>', views.editCurso),
     path('editaCurso/', views.editCursoPost),
     path('eliminarCurso/<codigo>', views.deleteCurso),
     path('buscar', views.buscarCurso),
+    path('autenticar',views.autenticar),
+    path('registrarUsuario',views.registrarF),
+    path('autenticar',views.autenticar),
+    path('listadoUsuarios',views.listadoUsuarios),
+    path('eliminarUsuario/<codigo>',views.eliminaUsuario),
+    path('editarUsuario/<codigo>',views.registrarF),
+    path('salir/',views.salir),
 ]
