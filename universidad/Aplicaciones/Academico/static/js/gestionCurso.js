5643ea3b79e4617c7c168a7fc0738ca9e1c3c1c5
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-03-23 17:12:37 -0400

    [ADD] datatable for user list

--- universidad/Aplicaciones/Academico/static/js/gestionCurso.js
+++ universidad/Aplicaciones/Academico/static/js/gestionCurso.js
@@ -57,5 +57,45 @@ jQuery(document).ready(function () {
         }
     };
 
+    const dataOptions1={
+        columnDefs:[
+            {className:"centered", targets:[0,1,2,3]},
+            {orderable:false, targets:[0,3]},
+            {searchable:false, targets:[3]},
+        ],
+        pageLength:5,
+        lengthMenu: [
+            [5,10, 25, -1],
+            [5, 10, 25, 'Todos'],
+        ],
+        destroy:true,
+        language: {
+            "sProcessing":    "Procesando...",
+            "sLengthMenu":    "Mostrar _MENU_ registros",
+            "sZeroRecords":   "No se encontraron resultados",
+            "sEmptyTable":    "Ningún dato disponible en esta tabla",
+            "sInfo":          "Mostrando _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty":     "Mostrando  0 de un total de 0 registros",
+            /*"sInfoFiltered":  "(filtrado de un total de _MAX_ registros)",*/
+            "sInfoFiltered":  "",
+            "sInfoPostFix":   "",
+            "sSearch":        "Buscar:",
+            "sUrl":           "",
+            "sInfoThousands":  ",",
+            "sLoadingRecords": "Cargando...",
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":    "Último",
+                "sNext":    "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "oAria": {
+                "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+        }
+    };
+
     jQuery('#example').DataTable(dataOptions);
+    jQuery('#example1').DataTable(dataOptions1);
 });
