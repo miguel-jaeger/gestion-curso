278419ec86ebf43f5b8f9f34d78f829aa37e0efb
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-04-04 16:03:02 -0400

    [UPDATE] change function to update and optimize code

--- universidad/Aplicaciones/Academico/views.py
+++ universidad/Aplicaciones/Academico/views.py
@@ -9,38 +9,46 @@ from .forms import cursoForm
 
 
 # Create your views here.
-def home(request):   
+def home(request):
     return render(request, "inicio.html")
 
+
 """ CURSOS FUNCTIONS"""
+
+
 @login_required
 def cursos(request):
     listadoCursos = Curso.objects.all()
     return render(request, "gestionCurso.html", {"cursos": listadoCursos})
 
+
 @login_required
 def addCursoF(request):
-    if request.method=="GET":        
+    if request.method == "GET":
         return render(request, "adicionarCurso.html")
     else:
-        try:                        
-            if 'id' in request.POST and request.POST['id'] != "":                
-                curso = get_object_or_404(Curso,pk=int(request.POST['id']))
-                form = cursoForm(request.POST, instance=curso)
-                form.save()
-                return redirect('/cursos')          
-            else:
-                form = cursoForm(request.POST)
-                form.save()
-                return redirect('/cursos')
-        except ValueError:
-            print(ValueError)
-            return render(request, "adicionarCurso.html",{"error":"Proveer datos válidos"})
-        
+        form = cursoForm(request.POST)
+        form.save()
+        return redirect('/cursos')
+
+
 @login_required
-def cursoDetalle(request,id):
-    curso = get_object_or_404(Curso,pk=id)
+def editCursoF(request, id):
+    if request.method == "GET":
+        curso = get_object_or_404(Curso, pk=int(id))
+        return render(request, "edicionCurso.html", {"curso": curso})
+    else:
+        curso = get_object_or_404(Curso, pk=int(id))
+        form = cursoForm(request.POST, instance=curso)
+        form.save()
+        return redirect('/cursos')
+
+
+@login_required
+def cursoDetalle(request, id):
+    curso = get_object_or_404(Curso, pk=id)
     return render(request, "cursoDetalles.html", {"curso": curso})
+
 
 @login_required
 def deleteCurso(request, id):
@@ -48,65 +56,75 @@ def deleteCurso(request, id):
     curso.delete()
     return redirect("/cursos")
 
+
 @login_required
 def buscarCurso(request):
     buscar = request.POST['buscar']
     filter = Curso.objects.filter(nombre__contains=buscar)
     return render(request, "gestionCurso.html", {"cursos": filter, "criterio": buscar})
 
+
 """USUARIOS FUNCTIONS"""
+
+
 def registrarF(request):
-    
-    if request.method=='GET':
+
+    if request.method == 'GET':
         return render(request, "registroUsuario.html")
     else:
 
-            #user = get_object_or_404(User,pk=id)
-            
-            if request.POST['password1']==request.POST['password2']:
-                try:
-                    #registrar usuario
-                    user=User.objects.create(username=request.POST['username'],password=request.POST['password1'],email=request.POST['email'],first_name=request.POST['first_name'],last_name=request.POST['last_name'])
-                    user.save()
-                    login(request,user)
-                    return redirect("/usuario/listado",{"msg":"Usuario registrado satisfactoriamente"})
-                except:
-                    return render(request, "registro.html", {"error": "El usuario ya existe"})
-            
-            else:
-                return render(request, "registro.html", {"error": "Las contraseñas no coinciden"})
+        # user = get_object_or_404(User,pk=id)
+
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # registrar usuario
+                user = User.objects.create(username=request.POST['username'], password=request.POST['password1'],
+                                           email=request.POST['email'], first_name=request.POST['first_name'], last_name=request.POST['last_name'])
+                user.save()
+                login(request, user)
+                return redirect("/usuario/listado", {"msg": "Usuario registrado satisfactoriamente"})
+            except:
+                return render(request, "registro.html", {"error": "El usuario ya existe"})
+
+        else:
+            return render(request, "registro.html", {"error": "Las contraseñas no coinciden"})
 
 
 def registrarUsuario(request):
-    
-    if request.method=='GET':
+
+    if request.method == 'GET':
         return render(request, "/usuario/listado.html")
     else:
-            if request.POST['password1']==request.POST['password2']:
-                try:
-                    #registrar usuario
-                    user=User.objects.create_user(username=request.POST['username'],password=request.POST['password1'],email=request.POST['email'])
-                    user.save()
-                    login(request,user)
-                    return redirect("/usuario/listado",{"msg":"Usuario registrado satisfactoriamente"})
-                except:
-                    return render(request, "registroUsuario.html", {"error": "El usuario ya existe"})
-            
-            else:
-                return render(request, "registroUsuario.html", {"error": "Las contraseñas no coinciden"})
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # registrar usuario
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'], email=request.POST['email'])
+                user.save()
+                login(request, user)
+                return redirect("/usuario/listado", {"msg": "Usuario registrado satisfactoriamente"})
+            except:
+                return render(request, "registroUsuario.html", {"error": "El usuario ya existe"})
+
+        else:
+            return render(request, "registroUsuario.html", {"error": "Las contraseñas no coinciden"})
+
 
 def editaUsuario(request):
-    User.objects.filter(id=request.POST['id']).update(username=request.POST['username'],first_name=request.POST['first_name'],last_name=request.POST['last_name'])
-    #user.save()
-    return redirect("/usuario/listado",{"msg":"Usuario actualizado satisfactoriamente"})
+    User.objects.filter(id=request.POST['id']).update(
+        username=request.POST['username'], first_name=request.POST['first_name'], last_name=request.POST['last_name'])
+    # user.save()
+    return redirect("/usuario/listado", {"msg": "Usuario actualizado satisfactoriamente"})
+
 
 @login_required
 def listadoUsuarios(request):
     listadoUsuarios = User.objects.all()
     return render(request, "listadoUsuarios.html", {"usuarios": listadoUsuarios})
 
+
 @login_required
-def buscarUsuario(request,id):
+def buscarUsuario(request, id):
     usuario = User.objects.get(id=id)
     return render(request, "editarUsuario.html", {"user": usuario})
 
@@ -116,23 +134,26 @@ def eliminaUsuario(request, id):
     usuario.delete()
     return redirect("/usuario/listado")
 
+
 def salir(request):
     logout(request)
     return redirect("/")
 
+
 def autenticar(request):
-    
-    if request.method=='GET':
+
+    if request.method == 'GET':
         return render(request, "registro.html")
     else:
-        user = authenticate(request,username=request.POST['username'],password=request.POST['password1'])
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password1'])
         if user is None:
-            return render(request, "registro.html",{"error":"Usuario o password incorrectoa"})
+            return render(request, "registro.html", {"error": "Usuario o password incorrectoa"})
         else:
-            login(request,user)
+            login(request, user)
             return redirect("/")
-        
 
-def error_404_view(request, exception):    
-    
-    return render(request,'404.html')
+
+def error_404_view(request, exception):
+
+    return render(request, '404.html')
