e7d87c1afc3f3db4c8181f5cf95df46ace912eac
miguel-jaeger <84285594+miguel-jaeger@users.noreply.github.com>
2023-04-04 11:57:55 -0400

    [UPDATE] organize URL

--- universidad/Aplicaciones/Academico/urls.py
+++ universidad/Aplicaciones/Academico/urls.py
@@ -5,20 +5,26 @@ from django.conf.urls import handler404
 
 urlpatterns = [
     path('', views.home,name="home"),
+    # CURSOS 
     path('cursos/', views.cursos,name="cursos"),
-    path('registrar/', views.addCursoF,name="registrar"),
-    path('registrarCurso/', views.addCurso,name="registrarCurso"),
-    path('editarCurso/<codigo>', views.editCurso,name="editarCurso"),
-    path('editaCurso/', views.editCursoPost,name="editarCursoPost"),
-    path('eliminarCurso/<codigo>', views.deleteCurso,name="eliminarCurso"),
-    path('buscar', views.buscarCurso,name="buscar"),
+    path('cursos/adicionar', views.addCursoF,name="registrar"),
+    path('cursos/<int:id>/',  views.cursoDetalle, name="cursosdetalle"),
+    path('cursos/<int:id>/editar',  views.addCursoF, name="cursoeditar"),
+    path('cursos/<int:id>/eliminar',  views.deleteCurso, name="cursoeliminar"),
+    #path('registrarCurso/', views.addCurso,name="registrarCurso"),
+    #path('editarCurso/<codigo>', views.editCurso,name="editarCurso"),
+    #path('editaCurso/', views.editCursoPost,name="editarCursoPost"),
+    #path('eliminarCurso/<codigo>', views.deleteCurso,name="eliminarCurso"),
+    #path('buscar', views.buscarCurso,name="buscar"),
+    # USUARIOS
     path('autenticar',views.autenticar,name="autenticar"),
-    path('registrarUsuario',views.registrarF,name="registrarUsuarioView"),
+    path('usuario',views.registrarF,name="registrarUsuario"),
     path('registrarUsuarioForm',views.registrarUsuario,name="registrarUsuarioForm"),
     path('autenticar/',views.autenticar,name="autenticar"),
-    path('listadoUsuarios',views.listadoUsuarios,name="listadoUsuarios"),
-    path('eliminarUsuario/<codigo>',views.eliminaUsuario,name="eliminarUsuario"),
-    path('editarUsuario/<codigo>',views.registrarF,name="editarUsuario"),
+    path('usuario/listado',views.listadoUsuarios, name="listadoUsuarios"),
+    path('usuario/<int:id>/eliminar',views.eliminaUsuario,name="eliminarUsuario"),
+    path('usuario/<int:id>/editar', views.buscarUsuario,name="editarUsuario"),
+    path('usuario/editar', views.editaUsuario,name="editarUsuario"),
     path('salir/',views.salir,name="salir"),
 
 ]
